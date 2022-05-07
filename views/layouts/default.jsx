4e9adefcd841cf602586638fa67f48c4560bf001
Alissa <alissamak@outlook.com>
2022-05-07 10:03:19 -0400

    added content

--- views/layouts/default.jsx
+++ views/layouts/default.jsx
@@ -4,9 +4,6 @@ function Default(html){
     return (
         <html lang="en">
         <head>
-            <meta charset="UTF-8" />
-            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <title>{html.title || 'Default'}</title>
         </head>
         <body>
