f9a55d6a1c944bf6cdbdec88232f5b4d461e623d
Alissa <alissamak@outlook.com>
2022-05-09 20:23:38 -0400

    added new post page

--- server.js
+++ server.js
@@ -6,10 +6,14 @@ const PORT = process.env.PORT;
 // console.log(PORT);
 
 //middleware
+//jsx middleware
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
+// show request middleware
 app.use(express.static('public'));
+//post request middleware
+app.use(express.urlencoded({extended: true}));
 
 //route
 app.get('/', (req, res) => {
@@ -25,7 +29,7 @@ app.get('*', (req, res) => {
         res.send('404')
 })
 
-//lisent for port number
+//listen for port number
 app.listen(PORT, () => {
     console.log('nomming at port', PORT);
 });
