9f2735eb235448f13d0faf33d459d9d691704b90
Alissa <alissamak@outlook.com>
2022-05-11 20:58:41 -0400

    added edit feature

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -32,6 +32,14 @@ breads.post('/', (req, res) => {
     res.redirect('/breads');
 })
 
+//edit index of breads
+breads.get('/:indexArray/edit', (req,res) => {
+    res.render('edit', {
+        bread: Bread[req.params.indexArray],
+        index: req.params.indexArray,
+    })
+})
+
 //show index of breads
 breads.get('/:arrayIndex', (req, res) => {
     // res.render('Show', {
@@ -52,6 +60,18 @@ breads.get('/:arrayIndex', (req, res) => {
 breads.delete('/:arrayIndex', (req, res) => {
     Bread.splice(req.params.arrayIndex, 1);
     res.status(303).redirect('/breads')
+})
+
+//update breads
+breads.put('/:arrayIndex', (req, res) => {
+    if(req.body.hasGluten === 'on'){
+        req.body.hasGluten = true;
+    }
+    else{
+        req.body.hasGluten = false;
+    }
+    Bread[req.params.arrayIndex] = req.body;
+    res.redirect(`/breads/${req.params.arrayIndex}`)
 })
 
 module.exports = breads;
