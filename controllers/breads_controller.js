5ddc280a12289799ec010404edef70b7e0e9dc8e
Alissa <alissamak@outlook.com>
2022-05-18 18:56:01 -0400

    added in delete and edit helpers

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -22,6 +22,36 @@ breads.get('/', (req, res) => {
     // );
 })
 
+//seed data
+breads.get('/data/seed', (req, res) => {
+    Bread.insertMany([
+        {
+          name: 'Rye',
+          hasGluten: true,
+          image: 'https://images.unsplash.com/photo-1595535873420-a599195b3f4a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80',
+        },
+        {
+          name: 'French',
+          hasGluten: true,
+          image: 'https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80',
+        },
+        {
+          name: 'Gluten Free',
+          hasGluten: false,
+          image: 'https://images.unsplash.com/photo-1546538490-0fe0a8eba4e6?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+        },
+        {
+          name: 'Pumpernickel',
+          hasGluten: true,
+          image: 'https://images.unsplash.com/photo-1586444248902-2f64eddc13df?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80',
+        }
+      ]
+      )
+    .then(createdBreads => {
+        res.redirect('/breads')
+    })
+})
+
 //create new page
 breads.get('/new', (req, res) => {
     res.render('new')
@@ -43,9 +73,15 @@ breads.post('/', (req, res) => {
 
 //edit index of breads
 breads.get('/:indexArray/edit', (req,res) => {
-    res.render('edit', {
-        bread: Bread[req.params.indexArray],
-        index: req.params.indexArray,
+    // res.render('edit', {
+    //     bread: Bread[req.params.indexArray],
+    //     index: req.params.indexArray,
+    // })
+    Bread.findById(req.params.indexArray)
+    .then(foundBread => {
+        res.render('edit', {
+            bread: foundBread,
+        })
     })
 })
 
@@ -76,8 +112,12 @@ breads.get('/:arrayIndex', (req, res) => {
 
 //delete breads
 breads.delete('/:arrayIndex', (req, res) => {
-    Bread.splice(req.params.arrayIndex, 1);
-    res.status(303).redirect('/breads')
+    // Bread.splice(req.params.arrayIndex, 1);
+    // res.status(303).redirect('/breads')
+    Bread.findByIdAndDelete(req.params.arrayIndex)
+    .then(deletedBread => {
+        res.status(303).redirect('/breads')
+    })
 })
 
 //update breads
@@ -88,8 +128,12 @@ breads.put('/:arrayIndex', (req, res) => {
     else{
         req.body.hasGluten = false;
     }
-    Bread[req.params.arrayIndex] = req.body;
-    res.redirect(`/breads/${req.params.arrayIndex}`)
+    // Bread[req.params.arrayIndex] = req.body;
+    // res.redirect(`/breads/${req.params.arrayIndex}`)
+    Bread.findByIdAndUpdate(req.params.arrayIndex, req.body, {new: true})
+    .then(updatedBread => {
+        res.redirect(`/breads/${req.params.arrayIndex}`)
+    })
 })
 
 module.exports = breads;
--- views/show.jsx
+++ views/show.jsx
@@ -16,8 +16,8 @@ function Show({bread, index}){
                 have gluten.
             </p>
             <img src={bread.image} alt={bread.name}></img>
-            <a href={`/breads/${index}/edit`}><button>Edit</button></a>
-            <form action={`/breads/${index}?_method=DELETE`} method="POST">
+            <a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
+            <form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
                 <input type="submit" value="DELETE"></input>
             </form>
             <a href='/breads'><button className='btn-primary'>Go home</button></a>
